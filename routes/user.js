ed8cfed5728c67fb7cdff22a8ce5b45fa2b80af0
Read Langworthy <rlangworthy@gmail.com>
2023-09-21 09:51:24 -0700

    update server.js and user route

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getUsers,
+    postUser,
+
+    login,
+    authenticate,
+    updatePassword,
+    logout,
+    
+    getUser,
+    updateUser,
+    deleteUser,
+} = require('../controllers/userController')
+
+const adminValidator = require('../middlewares/utils/validators')
+const protectedRoute = require('../middlewares/auth')
+
+// all on /user/ route
+router.route('/')
+    .get(protectedRoute, adminValidator, getUsers) // admin only route
+    .post(postUser) // not protected - create user (signup)
+
+router.route('/login')
+    .post(login) // not protected - explicit log in
+
+router.route('/authenticate')
+    .get(authenticate) // not protected - cookie-based log in
+
+router.route('/updatePassword')
+    .put(protectedRoute, updatePassword)
+
+router.route('/logout')
+    .get(protectedRoute, logout)
+
+router.route('/:userId')
+    .get(protectedRoute, getUser)
+    .put(protectedRoute, updateUser)
+    .delete(protectedRoute, deleteUser)
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,68 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors')
+const logger = require('./middlewares/logger')
+const errorHandler = require('./middlewares/error')
+const cookieParser = require('cookie-parser')
+const fileupload = require('express-fileupload')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const hpp = require('hpp')
+const xss = require('xss-clean')
+
+// imports the various routes of the server
+const user = require('./routes/user')
+const board = require('./routes/board')
+const card = require('./routes/card')
+
+const test = require('./routes/test')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:3000', // '*' is wildcard string, allowing any site
+    credentials: true,
+}))
+
+// middleware to use for all routes go here:
+app.use(cookieParser())
+app.use(fileupload()) // allows grabbing data out of uploaded files
+app.use(xss()) // sanitizes user input in post body, get queries, url params
+app.use(hpp())
+app.use(helmet({
+    contentSecurityPolicy: false, //disables these features of helmet
+    frameguard: false,
+})) // sets various http headers for security including CORS and frames
+app.use(logger)
+
+// this is a pretty relaxed rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // max api calls in window
+})
+app.use(limiter)
+
+// these are defining the routes
+app.use('/user', user)
+app.use('/board', board)
+app.use('/card', card)
+
+app.use('/test', test)
+
+const PORT = process.env.PORT || 5001
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
+
+// the errorHandler must be below routes to catch errors
+app.use(errorHandler)
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log( `Source: 'server.js' file` )
+    console.log( `Error: ${err.message}` ) 
+    console.log( err.stack ) 
+    server.close(() => process.exit(1)) // exits server if unhandled error
+})
